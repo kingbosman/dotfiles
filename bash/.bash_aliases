0034a679362473fb4c70cbc71faf7a362298a481
King Bosman <r.bosman.zakelijk@gmail.com>
2025-04-16 20:52:43 +0200

    bash include

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -3,12 +3,7 @@ alias ls='eza -aol --header --group-directories-first'
 alias myip="ip -f inet address | grep inet | grep -v 'lo$' | cut -d ' ' -f 6,13 && curl ifconfig.me && echo ' external ip'"
 alias e="xdg-open"
 
-## GIT ##
-alias gs="git status"
-alias gp="git pull"
-alias gb="git branch"
-alias gd="git diff"
-alias gc="git checkout"
+## GIT: Keep forgetting this ##
 gcb() {
  if [ -z "$1" ] || [ -z "$2" ]
         then
@@ -19,20 +14,10 @@ gcb() {
   git push --set-upstream origin $1
         fi
 }
-gcp() {
- if [ -z "$1" ] 
- then
-  echo 'Commit message is required use gcp {{message}}'
- else 
-  echo 'trying to add files and push with commit message ' $1
-  git add .
-  git commit -m $1
-  git push
- fi
-}
 
 ## Code ##
 alias v="nvim"
+alias vim="nvim"
 alias sail='./vendor/bin/sail'
 alias sa='./vendor/bin/sail artisan'
 alias a='php artisan'
--- install_linux_mint.sh
+++ install_linux_mint.sh
@@ -1,26 +1,33 @@
 #! /usr/bin/env bash
 
-# creating temp dir so it will only link
+# creating.generated-install dir so it will only link
 # once the install script has been called
 # This will also allow for different install scripts
-rm -rf .temp
-mkdir .temp
+rm -rf .generated-install
+mkdir .generated-install
 
 DOTFILES=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 echo 'linking nvim'
-cp -r $DOTFILES/nvim $DOTFILES/.temp/nvim 
+cp -r $DOTFILES/nvim $DOTFILES/.generated-install/nvim 
 rm -rf $HOME/.config/nvim
-ln -s $DOTFILES/.temp/nvim $HOME/.config/nvim
+ln -s $DOTFILES/.generated-install/nvim $HOME/.config/nvim
 
 echo 'linking kitty'
-cp -r $DOTFILES/kitty $DOTFILES/.temp/kitty 
+cp -r $DOTFILES/kitty $DOTFILES/.generated-install/kitty 
 rm -rf $HOME/.config/kitty
-ln -s $DOTFILES/.temp/kitty $HOME/.config/kitty
+ln -s $DOTFILES/.generated-install/kitty $HOME/.config/kitty
 
 echo 'linking tmux'
-cp -r $DOTFILES/tmux $DOTFILES/.temp/tmux 
+cp -r $DOTFILES/tmux $DOTFILES/.generated-install/tmux 
 rm -rf $HOME/.config/tmux
-ln -s $DOTFILES/.temp/tmux $HOME/.config/tmux
+ln -s $DOTFILES/.generated-install/tmux $HOME/.config/tmux
+
+echo 'linking bash files'
+cp -r $DOTFILES/bash $DOTFILES/.generated-install/bash 
+rm -rf $HOME/.config/bash
+rm -rf $HOME/.config/.bashrc
+ln -s $DOTFILES/.generated-install/bash $HOME/.config/bash
+ln -s $DOTFILES/.generated-install/bash $HOME/.config/.bashrc
 
 echo 'Done linking all files!'
