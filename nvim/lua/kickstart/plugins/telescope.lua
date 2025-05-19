6fbb4feedd061e2bf11db17b8166d9e112e6732a
King Bosman <r.bosman.zakelijk@gmail.com>
2025-05-19 14:52:21 +0200

    live grep auto escape characters

--- nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/lua/kickstart/plugins/telescope.lua
@@ -74,12 +74,13 @@ return {
 
 			-- See `:help telescope.builtin`
 			local builtin = require("telescope.builtin")
+			local conf = require("telescope.config").values
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sF", builtin.find_files, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			-- vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -94,12 +95,27 @@ return {
 				}))
 			end, { desc = "[/] Fuzzily search in current buffer" })
 
+			-- Edited live grep to escape
+			vim.keymap.set("n", "<leader>sg", function()
+				local vimgrep_arguments = vim.deepcopy(conf.vimgrep_arguments)
+				table.insert(vimgrep_arguments, "--fixed-strings")
+
+				builtin.live_grep({
+					prompt_title = "Live Grep in Files",
+					vimgrep_arguments = vimgrep_arguments,
+				})
+			end, { desc = "[S]earch [g]rep in Files" })
+
 			-- It's also possible to pass additional configuration options.
 			--  See `:help telescope.builtin.live_grep()` for information about particular keys
 			vim.keymap.set("n", "<leader>s/", function()
+				local vimgrep_arguments = vim.deepcopy(conf.vimgrep_arguments)
+				table.insert(vimgrep_arguments, "--fixed-strings")
+
 				builtin.live_grep({
 					grep_open_files = true,
 					prompt_title = "Live Grep in Open Files",
+					vimgrep_arguments = vimgrep_arguments,
 				})
 			end, { desc = "[S]earch [/] in Open Files" })
 
