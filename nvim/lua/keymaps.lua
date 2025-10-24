7c60c9743cda791a014f465c7190c2b936aeef4b
King Bosman <r.bosman.zakelijk@gmail.com>
2025-10-24 09:06:39 +0200

    add todo telescope shortcut

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -19,6 +19,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Todo
+vim.keymap.set("n", "<leader>T", ":TodoTelescope<CR>", { desc = "Open [T]odo List in telescope" })
+
 -- Keep cursor in place when yanking
 vim.keymap.set("v", "y", "myy`y")
 
