798f9ed2374febb73494325a325e3ac79d59e0f9
King Bosman <r.bosman.zakelijk@gmail.com>
2026-01-23 20:56:21 +0100

    zoxide in bashrc and filetype change

--- nvim/lua/file-types.lua
+++ nvim/lua/file-types.lua
@@ -1,13 +1,20 @@
--- add custom extensions here for custom file extension highlighting
 vim.filetype.add({
 	extension = {
 		neon = "yaml",
 		sqlfluff = "ini",
 		conf = "ini",
 	},
+	-- Use 'filename' for exact matches
+	filename = {
+		[".zshrc"] = "sh",
+		[".bashrc"] = "sh",
+		[".env"] = "sh",
+	},
+	-- Use 'pattern' for things with suffixes or wildcards
 	pattern = {
-		["%.env$"] = "sh",        -- matches exactly .env
-		["%.env%..*"] = "sh",      -- matches .env.test, .env.local, etc.
-		["%.env_.*"] = "sh",       -- matches .env_test, .env_tesdf, etc.
+		["%.env%..*"] = "sh",
+		["%.env_.*"] = "sh",
+		["%.bashrc.*"] = "sh",
+		["%.zshrc.*"] = "sh",
 	},
 })
