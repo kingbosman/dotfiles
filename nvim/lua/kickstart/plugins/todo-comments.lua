22edc0f21b06e22b8eb060cdff7d9b53d99dfc2d
King Bosman <r.bosman.zakelijk@gmail.com>
2026-02-20 10:14:59 +0100

    todo crashing fix exclude some basic folder

--- nvim/lua/kickstart/plugins/todo-comments.lua
+++ nvim/lua/kickstart/plugins/todo-comments.lua
@@ -1,10 +1,25 @@
--- Highlight todo, notes, etc in comments
 return {
 	{
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
+		opts = {
+			signs = false,
+			search = {
+				command = "rg",
+				args = {
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--glob=!node_modules",
+					"--glob=!dist",
+					"--glob=!public",
+					"--glob=!build",
+					"--glob=!*.min.js",
+				},
+			},
+		},
 	},
 }
--- vim: ts=2 sts=2 sw=2 et
